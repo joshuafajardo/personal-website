7781ceb923b0b5ad30ae99330553943582c500cc
Joshua Fajardo <jdfajardo@berkeley.edu>
2020-04-03 01:15:31 -0700

    Starting to implement react

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var sass = require ('gulp-sass');
-var minifyCSS = require('gulp-clean-css');
-var uglify = require('gulp-uglify')
-var rename = require('gulp-rename');
-var changed = require('gulp-changed');
